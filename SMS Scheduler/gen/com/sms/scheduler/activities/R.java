dd79bc1f888437dccda8ac10576971289fe95401
Rahul <rahul.arox@gmail.com>
2012-10-22 00:44:42 +0530

    check mdpi for png again

--- SMS Scheduler/gen/com/sms/scheduler/activities/R.java
+++ SMS Scheduler/gen/com/sms/scheduler/activities/R.java
@@ -21,10 +21,10 @@ public final class R {
         public static final int sch_month_date_text_size=0x7f040007;
     }
     public static final class drawable {
-        public static final int 8656551992073256605=0x7f020000;
-        public static final int ic_action_search=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int ic_new_message=0x7f020003;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_new_message=0x7f020002;
+        public static final int temp_icon=0x7f020003;
     }
     public static final class id {
         public static final int b_ll_add_contact_contacts=0x7f080002;
